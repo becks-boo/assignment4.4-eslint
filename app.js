edf6e0e5522b019576d560627c545e77f770cf3e
Igor Stein <istein@EN2910108.fritz.box>
2022-01-21 11:35:30 +0100

    task 3 finished, search query working

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const Twig = require("twig")
 const db = require('./src/Database.js')
 const Search = require('./Search');
 
+app.use(express.urlencoded());
+
 const port = 3000
 app.use(express.static('public'))
 app.set('view engine', 'twig')
@@ -23,9 +25,40 @@ app.post('/search', (req, res) => {
     // Saves the user text input
     const searchInput = req.body.search;
     // Saves the user radio btn choice
-    const columnChoice = req.body.radioBtn;
+    const radioBtnSelection = req.body.radioBtn;
+    // Initialize empty string variable to change later depending on radioBtnSelection
+    let columnChoice = "";
 
-    const searchQuery = `SELECT * FROM search WHERE ${columnChoice} LIKE ?`;
+    // Example: SELECT * FROM search WHERE title LIKE "%Gather%"
+    let searchQuery;
+
+    if (radioBtnSelection === "all-titles") {
+        // If user selects radio btn "Show all titles"
+        searchQuery = `SELECT * FROM search`;
+
+        /* In this case a separate method is needed because this query is different than the others,
+        no params */
+        db.all(searchQuery, (err, rows) => {
+            if (err) {
+                console.log(err);
+            }
+
+            res.render('search.twig', { outputs: rows });
+        });
+    } else if (radioBtnSelection === "filter-title") {
+        // If user selects radio btn "Filter by title"
+        columnChoice = "title";
+        searchQuery = `SELECT * FROM search WHERE ${columnChoice} LIKE ?`;
+    } else if (radioBtnSelection === "filter-url") {
+        // If user selects radio btn "Filter by URL"
+        columnChoice = "url";
+        searchQuery = `SELECT * FROM search WHERE ${columnChoice} LIKE ?`;
+    } else {
+        // If user selects radio btn "Filter by ID"
+        columnChoice = "id";
+        searchQuery = `SELECT * FROM search WHERE ${columnChoice} LIKE ?`;
+    }
+
 
     db.all(searchQuery, [`%${searchInput}%`], (err, rows) => {
         if (err) {
@@ -36,12 +69,8 @@ app.post('/search', (req, res) => {
     })
 })
 
-app.all('*', (req, res) => {
-    res.status(400).send("<h1>404 Page Not Found</h1>");
-});
-
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}/search`)
 })
 
 module.exports = app;
